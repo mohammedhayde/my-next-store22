801f39984d449674ef77d5cba05392b5e08ed797
mohammedhayde <hamodeiraq390@gmail.com>
2024-05-28 22:27:06 +0300

    Updatenew

--- src/app/services/fetchProductsByCategoryName.tsx
+++ src/app/services/fetchProductsByCategoryName.tsx
@@ -2,8 +2,6 @@ import { Product } from '@/app/types/typesProduct';
 
 export const fetchProductsByCategoryName = async (categoryName: string): Promise<Product[]> => {
   const response = await fetch(`${process.env.API_URL}/api/ProductsController21/ByCategoryName/${categoryName}?pageNumber=1&pageSize=10`);
-
-  
   const products: Product[] = await response.json();
   return products;
 };
